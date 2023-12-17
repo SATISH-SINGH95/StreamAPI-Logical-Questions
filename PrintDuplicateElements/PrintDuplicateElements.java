79ee87e50e59c2c95f50df66a916ed1ed7ddc4b7
satish <sk95satish@gmail.com>
2023-12-17 22:25:45 +0530

    Print Duplicate Elements

---
+++ PrintDuplicateElements/PrintDuplicateElements.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class PrintDuplicateElements {
+
+    public static void main(String[] args) {
+        List<Integer> n = Arrays.asList(1,2,3,1,2,6,2,7,2);
+
+        Set<Integer> collect = n.stream().filter(x ->n.stream().filter(a-> a==x).count()>1).collect(Collectors.toSet());
+        System.out.println(collect);
+    }
+    
+}
